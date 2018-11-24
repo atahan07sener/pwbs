f09b8befa761f97ee7d11a0d17e8baf05a7e370b
André Yuhai <burak.kaymakci@bil.omu.edu.tr>
2018-11-24 14:01:14 +0100

    improved javascript

--- bootstrap/javascript.js
+++ bootstrap/javascript.js
@@ -1,15 +1,24 @@
 var counter = 1;
-function addProduct(quantity, price,productName){
-	var sum = quantity * price;
+function addProduct(quantity, price,productName,taxRate){
+	var grossPrice = quantity * price;
+	var selectedQtyType = $('#qtySelect option:selected').text();
+	var selectedQtyValue = $('#qtySelect option:selected').val();
+
 	var trow = "<tr id=row"+counter+">\
 	<td>"+counter+"</td>\
 	<td>"+productName+"</td>\
-	<td>"+quantity+"</td>\
-	<td>"+price+" $</td>\
-	<td>"+sum+" $</td>\
+	<td>"+quantity+" "+selectedQtyType+"</td>\
+	<td>$"+price+" / "+selectedQtyType+"</td>\
+	<td>$"+grossPrice+"</td>\
+	<td>%"+taxRate+"</td>\
+	<td>$"+getNetPrice(grossPrice, taxRate)+"</td>\
 	</tr>";
 	$('#table tbody').append(trow);
 	counter++;
+}
+
+function getNetPrice(grossPrice, taxRate){
+	return grossPrice - grossPrice * (taxRate/100);
 }
 
 function deleteRow(rowNumber){
@@ -18,7 +27,5 @@ function deleteRow(rowNumber){
 		counter--;
 	}else if(rowNumber == 1){
 		var k = $('#table tbody tr').length;
-		for(
 	}
-	
 }
