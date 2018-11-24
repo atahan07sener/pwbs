e6d5958ee57c6fb0dbe01d57a89e77f25511c1c1
André Yuhai <burak.kaymakci@bil.omu.edu.tr>
2018-11-24 11:16:17 +0100

    td change

--- bootstrap/javascript.js
+++ bootstrap/javascript.js
@@ -2,20 +2,23 @@ var counter = 1;
 function addProduct(quantity, price,productName){
 	var sum = quantity * price;
 	var trow = "<tr id=row"+counter+">\
-							<td>"+counter+"</td>\
-							<td>"+productName+"</td>\
-							<td>"+quantity+"</td>\
-							<td>"+price+"</td>\
-							<td>"+sum+"</td>\
-						</tr>";
+	<td>"+counter+"</td>\
+	<td>"+productName+"</td>\
+	<td>"+quantity+"</td>\
+	<td>"+price+" $</td>\
+	<td>"+sum+" $</td>\
+	</tr>";
 	$('#table tbody').append(trow);
 	counter++;
 }
 
 function deleteRow(rowNumber){
-	$('#row'+rowNumber).remove();
-	counter--;
+	if (rowNumber == counter - 1){
+		$('#row'+rowNumber).remove();
+		counter--;
+	}else if(rowNumber == 1){
+		var k = $('#table tbody tr').length;
+		for(
+	}
+	
 }
-
-
-
