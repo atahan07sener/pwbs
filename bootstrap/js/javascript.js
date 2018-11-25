23b085f39dfe18854256c00459ea129f1cb2157c
André Yuhai <burak.kaymakci@bil.omu.edu.tr>
2018-11-25 17:46:48 +0100

    some more changes

--- bootstrap/js/javascript.js
+++ bootstrap/js/javascript.js
@@ -2,12 +2,11 @@ var rowCount = 0;
 
 function addProduct(quantity, price,productName,taxRate){
 
-	var grossPrice = (quantity * price);
-	var netPrice = getNetPrice(grossPrice, taxRate);
-	var tax = grossPrice * (taxRate/100);
-	var sTax = tax.toPrecision(4);
-	var sGrossPrice = grossPrice.toPrecision(4);
-	var sNetPrice = netPrice.toPrecision(4);
+	var grossPrice = accounting.toFixed((quantity * price),2);
+	var netPrice = accounting.toFixed(getNetPrice(grossPrice, taxRate),2);
+	var tax = accounting.toFixed(grossPrice * (taxRate/100),2);
+	console.log(tax);
+	
 	var selectedQtyType = $('#qtySelect option:selected').text();
 	var selectedQtyValue = $('#qtySelect option:selected').val();
 	rowCount++;
@@ -17,9 +16,9 @@ function addProduct(quantity, price,productName,taxRate){
 	<td>"+productName+"</td>\
 	<td>"+quantity+" "+selectedQtyType+"</td>\
 	<td>$"+price+" / "+selectedQtyType+"</td>\
-	<td>$"+sGrossPrice+"</td>\
+	<td>$"+grossPrice+"</td>\
 	<td>%"+taxRate+"</td>\
-	<td>$"+sNetPrice+"</td>\
+	<td>$"+netPrice+"</td>\
 	</tr>";
 	$('#table tbody').append(trow);
 
@@ -32,24 +31,25 @@ function addProduct(quantity, price,productName,taxRate){
 			$('<div>',{
 				id: 'totalNPrice',
 				class: 'offset-lg-6 offset-md-4 col-lg-3 col-md-4 col-sm-6 col-12'
-			}).html('Total Net Price: $<span id=\"spanGrossPrice\">'+sNetPrice+'</span>'),
+			}).html('Total Net Price: $<span id=\"spanNetPrice\">'+netPrice+'</span>'),
 			$('<div>',{
 				id: 'totalTax',
 				class: 'col-lg-3 col-md-4 col-sm-6 col-12'
-			}).html('Total Tax: $<span id=\"spanTax\">'+sTax+'</span>'))
+			}).html('Total Tax: $<span id=\"spanTax\">'+tax+'</span>'))
 			)
 	}else{
-		addTotals(grossPrice, tax);
+		console.log('burada');
+		addTotals(parseFloat(netPrice), parseFloat(tax));
 	}
 }
 
-function addTotals(grossPrice, tax){
+function addTotals(netPrice, tax){
 
-	var floatGPrice = parseFloat($("#spanGrossPrice").html());
+	var floatNPrice = parseFloat($("#spanNetPrice").html());
 	var floatTax = parseFloat($("#spanTax").html());
 
-	$('#spanGrossPrice').html((grossPrice + floatGPrice).toPrecision(4));
-	$('#spanTax').html((tax + floatTax).toPrecision(4));
+	$('#spanNetPrice').html(accounting.toFixed((netPrice + floatNPrice)));
+	$('#spanTax').html(accounting.toFixed((tax + floatTax), 2));
 }
 
 function addtoTotalGrossPrice(){
