e09da55a736f55078373fdd0c381eaf479b16ca2
Atahan Şener <atahan.sener@users.noreply.github.com>
2018-12-02 23:33:17 +0100

    sum req.

--- myJavaScript.js
+++ myJavaScript.js
@@ -34,19 +34,17 @@ function Function5(){
 	console.log(name);
 }
 
-window.sumInputs = function() {
-    var inputs = document.getElementsByTagName('input'),
-        result = document.getElementById('total'),
-        sum = 0;
+function = Function6() {
+	{
+  var sum = 0;
+  var inputs = document.getElementsByTagName("input");
+  for(i = 0; i <= inputs.length; i++)
+  {
+   if( inputs[i].name == 'qty'+i)
+   {
+     sum += parseInt(input[i].value);
+   }
+  }
+  console.log(sum)
 
-    for(var i=0; i<inputs.length; i++) {
-        var ip = inputs[i];
-
-        if (ip.name && ip.name.indexOf("total") < 0) {
-            sum += parseInt(ip.value) || 0;
-        }
-
-    }
-
-    result.value = sum;
-}​   
+}
